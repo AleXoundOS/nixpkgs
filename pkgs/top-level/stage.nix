4d096482471fd12847bedb6cae670e03fb8137ec
Alexander Tomokhov <alexoundos@gmail.com>
2021-12-01 11:35:26 +0300

    FIXME: pkgsStatic does not force musl

--- pkgs/top-level/stage.nix
+++ pkgs/top-level/stage.nix
@@ -234,12 +234,13 @@ let
       crossSystem = {
         isStatic = true;
         parsed = stdenv.hostPlatform.parsed // {
-          abi = {
-            gnu = lib.systems.parse.abis.musl;
-            gnueabi = lib.systems.parse.abis.musleabi;
-            gnueabihf = lib.systems.parse.abis.musleabihf;
-          }.${stdenv.hostPlatform.parsed.abi.name}
-            or lib.systems.parse.abis.musl;
+          # FIXME
+          #abi = {
+          #  gnu = lib.systems.parse.abis.musl;
+          #  gnueabi = lib.systems.parse.abis.musleabi;
+          #  gnueabihf = lib.systems.parse.abis.musleabihf;
+          #}.${stdenv.hostPlatform.parsed.abi.name}
+          #  or lib.systems.parse.abis.musl;
         };
         gcc = stdenv.hostPlatform.gcc;
       } // lib.optionalAttrs (stdenv.hostPlatform.system == "powerpc64-linux") {
